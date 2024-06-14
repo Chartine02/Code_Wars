7563f430a5d79d727f8a8a79d17c74bab6434628
Chartine02 <noellachartine125@gmail.com>
2024-06-14 17:43:41 +0200

    Promise state

--- 6kyu/promise-state.js
+++ 6kyu/promise-state.js
@@ -1,12 +1,12 @@
 async function getState(promise) {
-  const state = await promise.status;
-  if (state === "pending") {
-    console.log("Promise is pending");
-  } else if (state === "fulfilled") {
-    console.log("Promise is fulfilled");
-  } else if (state === "rejected") {
-    console.log("Promise is rejected");
-  }
+  const pendingPromise = new Promise((resolve) =>
+    setTimeout(() => resolve("pending"), 0)
+  );
+  return Promise.race([
+    promise.then(
+      () => "fulfilled",
+      () => "rejected"
+    ),
+    pendingPromise,
+  ]);
 }
-
-getState(promise);
